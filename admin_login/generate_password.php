d8d8701b274463d6d976fdf9aac04fa3dd2ba7e4
kart9071 <karthikakala13@gmail.com>
2024-03-03 01:42:36 +0530

    this is new

--- admin_login/generate_password.php
+++ admin_login/generate_password.php
@@ -1,11 +1,4 @@
 <?php
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 // Function to generate password
 function generatePassword($length = 10) {
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -27,37 +20,6 @@ function hashPassword($password) {
     return password_hash($password, PASSWORD_DEFAULT);
 }
 
-// Function to send email using SMTP
-function sendEmail($to, $subject, $message) {
-    $mail = new PHPMailer(true);
-
-    try {
-        // SMTP settings
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = false;
-        $mail->Username = 'karthikshetty200313@gmail.com'; // Your Gmail address
-        $mail->Password = 'Hello@887'; // Your Gmail password
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-        // Email headers
-        $mail->setFrom('karthikshetty200313@gmail.com', 'Karthik Shetty'); // Replace with your name
-        $mail->addAddress($to);
-
-        // Content
-        $mail->isHTML(false);
-        $mail->Subject = $subject;
-        $mail->Body = $message;
-
-        // Send email
-        $mail->send();
-        return true;
-    } catch (Exception $e) {
-        return false;
-    }
-}
-
 // Generate a random username
 $username = "admin1" . rand(10, 101);
 echo "Generated Username is: " . $username . "\n";
@@ -77,16 +39,6 @@ writeAdminCredentialsToFile($username, $hashedPassword, $file);
 fclose($file);
 
 echo "Admin credentials file updated successfully.\n";
-
-// Send the generated credentials to your email
-$to = "karthikshetty200313@gmail.com"; // Replace with your mobile's email to SMS gateway
-$subject = "Hello".$username."Thank you for the login"."\n"."Please refer this email message for the admin credentials details of your application";
-$message = "Username: $username \n Password: $password";
-if (sendEmail($to, $subject, $message)) {
-    echo "Credentials sent to your mobile successfully.\n";
-} else {
-    echo "Failed to send credentials to your mobile.\n";
-}
 
 ?>
 
